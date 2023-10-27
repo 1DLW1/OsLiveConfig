ea2a6254801d782b6669b5a9fcfeac0a67421a66
1DLW1 <88008917+1DLW1@users.noreply.github.com>
2023-10-27 01:33:42 +0100

    Add files via upload

--- liveConfig.py
+++ liveConfig.py
@@ -16,7 +16,6 @@ def timezone():
     #turn on ntp sync
     subprocess.run('sudo timedatectl set-ntp on', shell=True)
 
-
 #Folder of the network adapter driver
 carpeta='rtl8812au-5.6.4.2'
 ruta_carpeta = os.path.join(os.getcwd(), carpeta)
@@ -24,9 +23,12 @@ ruta_carpeta = os.path.join(os.getcwd(), carpeta)
 #Installation of network adapter driver
 if os.path.exists(ruta_carpeta):
     os.chdir(carpeta)
-    subprocess.run('sudo dkms_install && sudo make && sudo make install',shell=True)
+    subprocess.run('sudo make dkms_install && sudo make && sudo make install',shell=True)
     #Assignment of the essid of the interface
     subprocess.run('sudo iwconfig wlan0 essid off && sudo iwconfig wlan0 essid TpLink && sudo ifconfig wlan0 up',shell=True)
+    print('\n')
+    timezone()
+    print('\n')
     subprocess.run('sudo apt update && sudo apt upgrade',shell=True)
 
 else:
